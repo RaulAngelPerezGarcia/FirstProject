eca1499b4fa8e03c47c03d23f16e0a1685c0ec16
Raul Perez <zerepangel@gmail.com>
2016-10-11 21:53:45 +0200

    Created the server and added a bunch of stuff

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+console.log('Hello Kristianworld');
+
+$(function() {
+	$("input:file").change(function (){
+		var fileName = $(this).val();
+		console.log(fileName);
+		 $.ajax({
+			url: '/file',
+			type: 'POST',
+			// contentType: 'application/json',
+			data: {'fileName': fileName},
+			success: function( data, textStatus, jQxhr ){
+				console.log(data.message);
+			},
+			error: function( jqXhr, textStatus, errorThrown ){
+				console.log( errorThrown );
+			}
+		});
+	});
+});
