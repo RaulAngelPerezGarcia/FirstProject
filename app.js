fdcde25ce5545242390f04b5f5224170482d7b32
Raul Perez <zerepangel@gmail.com>
2016-10-15 16:23:41 +0200

    Edited some files and tried to add mongose

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/*
-
 // server.js
 
 // BASE SETUP
@@ -10,12 +8,18 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+var fs = require('fs');										// From app2.js
+var mongoose = require('mongoose');							// From app2.js
+var Schema = mongoose.Schema;								// From app2.js
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// connect to mongo
+mongoose.connect('localhost', 'testing_storeImg');         // From app2.js
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -40,88 +44,37 @@ router.route('/file').post( function(req, res) {
 // all of our routes will be prefixed with /api
 app.use('/', router);
 
+// Error handlers
+// =============================================================================
+// Catch 404 adn forwar to error handler
+app.use(function(req, res, next){
+	var err = new Error('No encontrado');
+	err.status = 404;
+	next(err);
+});
+// Development error handler
+// will print stacktrace
+if (app.get('env') === 'development'){
+	app.use(function(req, res, next){
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// Production error handler
+// no stacltraces leaked to user
+app.use(function(req, res, next){
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: {}
+		});
+	});
+
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
-
-*/
-
-// =============================================================================
-// =============================================================================
-// =============================================================================
-// =============================================================================
-// =============================================================================
-
-/**
- * Module dependencies
- */
-
-var express = require('express');
-var fs = require('fs');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-// img path
-var imgPath = '/path/to/some/img.png';
-
-// connect to mongo
-mongoose.connect('localhost', 'testing_storeImg');
-
-// example schema
-var schema = new Schema({
-    img: { data: Buffer, contentType: String }
-});
-
-// our model
-var A = mongoose.model('A', schema);
-
-mongoose.connection.on('open', function () {
-  console.error('mongo is open');
-
-  // empty the collection
-  A.remove(function (err) {
-    if (err) throw err;
-
-    console.error('removed old docs');
-
-    // store an img in binary in mongo
-    var a = new A;
-    a.img.data = fs.readFileSync(imgPath);
-    a.img.contentType = 'image/png';
-    a.save(function (err, a) {
-      if (err) throw err;
-
-      console.error('saved img to mongo');
-
-      // start a demo server
-      var server = express.createServer();
-      server.get('/', function (req, res, next) {
-        A.findById(a, function (err, doc) {
-          if (err) return next(err);
-          res.contentType(doc.img.contentType);
-          res.send(doc.img.data);
-        });
-      });
-
-      server.on('close', function () {
-        console.error('dropping db');
-        mongoose.connection.db.dropDatabase(function () {
-          console.error('closing db connection');
-          mongoose.connection.close();
-        });
-      });
-
-      server.listen(3333, function (err) {
-        var address = server.address();
-        console.error('server listening on http://%s:%d', address.address, address.port);
-        console.error('press CTRL+C to exit');
-      });
-
-      process.on('SIGINT', function () {
-        server.close();
-      });
-    });
-  });
-
-});
+console.log('Server started at port: ' + port);
