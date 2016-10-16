d48d2edd95f2e054e443b1b8d0cc376e83fc4999
Raul Perez <zerepangel@gmail.com>
2016-10-16 18:08:01 +0200

    Fixed the main.js so that it would upload a picture, missing the server part

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,47 @@
+$('.upload-btn').on('click',function(){
+	$('#upload-input').click();
+	$('.progress-bar').text('0%');
+	$('.progress-bar').width('0%');
+});
+
+
+$('#upload-input').on('change', function(){
+
+	var files = $(this).get(0).files;
+
+		// One or more files selected, process the file upload
+	if (files.length > 0){
+
+		// Create a FormData object which will be sent as 
+		// the data payload in the AJAX request
+		var formData = new FormData();
+
+		// loop through all the selected files
+		for (var i = 0; i < files.length; i++){
+			var file = files[i];
+
+			// Add the files to formData object for the data payload
+			formData.append('uploads[]', file, file.name)
+		}
+
+		$.ajax({
+			url: '/upload',
+			type: 'POST',
+			data: formData,
+			processData: false,
+			contentType: false,
+			success: function(data){
+				console.log('Dear user, the upload was successful')
+			}			
+		});
+	}
+
+});
+
+
+
+/** This is what I had from before
+
 $(function() {
 	$("input:file").change(function (){
 		var fileName = $(this).val();
@@ -16,3 +60,4 @@ $(function() {
 		});
 	});
 });
+*/
