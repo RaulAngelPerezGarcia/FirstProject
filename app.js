23bf3143567877993d6468fe9bc22b86223c4b30
Raul Perez <zerepangel@gmail.com>
2016-10-16 18:39:58 +0200

    I made it! The file uploader is done! User can now upload a picture and we can save it into our server in the /Uploads folder

--- app.js
+++ app.js
@@ -7,42 +7,82 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var formidable = require('formidable');
 var fs = require('fs');										// From app2.js
-var mongoose = require('mongoose');							// From app2.js
-var Schema = mongoose.Schema;								// From app2.js
+//var mongoose = require('mongoose');							// From app2.js
+//var Schema = mongoose.Schema;								// From app2.js
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 // connect to mongo
 //mongoose.connect('localhost', 'testing_storeImg');         // From app2.js
 
 var port = process.env.PORT || 8080;        // set our port
 
+//app.use(express.static(path.join(__dirname, 'public')));
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	res.sendFile(__dirname + '/views//index.html');
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+/**
 router.route('/file').post( function(req, res) {
 	var fileName = req.body.fileName;
 	var lefil = req.body;
 	res.json({ message: 'hooray! We got the file! ' + fileName+ ' '+ lefil});
 });
-
+*/
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/', router);
+app.post('/upload', function(req, res){
+	console.log('We are sending a post');
+
+	// create an incoming form object
+	var form = new formidable.IncomingForm();
+
+	// specify that we want to allow the user to upload multiple files in a single request
+	form.multiples = true;
+
+	// store all uploads in the /uploads directory
+	form.uploadDir = path.join(__dirname, '/uploads');
+	console.log(form);
+
+	// every time a file has been uploaded successfully,
+	// rename it to it's orignal name
+	form.on('file', function(field, file) {
+		fs.rename(file.path, path.join(form.uploadDir, file.name));
+	});
+
+	// log any errors that occur
+	form.on('error', function(err) {
+		console.log('An error has occured: \n' + err);
+	});
+
+	// once all the files have been uploaded, send a response to the client
+	form.on('end', function() {
+		res.end('success');
+		console.log('form.on')
+	});
+
+	// parse the incoming request containing the form data
+	form.parse(req);
+
+});
+
+
+
 
 // Error handlers
 // =============================================================================
@@ -76,5 +116,7 @@ app.use(function(req, res, next){
 
 // START THE SERVER
 // =============================================================================
-app.listen(port);
-console.log('Server started at port: ' + port);
+
+var server = app.listen(port, function(){
+	console.log('Server listening on port 8080');
+});
